d07a55a07a90f19ec2db1093b1cce8a3a86d01d1
yashasvi <yashpant20@gmail.com>
2020-05-10 20:08:13 +0545

    Optimized and modified for hackerrank

--- 007.py
+++ 007.py
@@ -10,18 +10,43 @@ What is the 10001st prime number?
 import math
 
 
-def prime():
-    number = 2
-    count = 1
+def prime(n, prime_list):
+    number = 3
+    count = 2
+
+    prime_list_length = len(prime_list)
+
+    if (n == 1):
+        return 2
+
+    if prime_list_length > 1:
+        if prime_list_length == n:
+            return prime_list[n - 1]
+        if prime_list_length > n:
+            return prime_list[n - 1]
+        elif prime_list_length < n:
+            count = prime_list_length + 1
+            number = prime_list[prime_list_length - 1] + 2
+
     while True:
-        is_prime = all(number % i != 0 for i in range(2, int(math.sqrt(number) + 1)))
+        is_prime = True
+        for i in range(2, int(math.sqrt(number) + 1)):
+            if number % i == 0:
+                is_prime = False
+                break
         if is_prime:
-            if count == 10001:
+            prime_list.append(number)
+            if count == n:
                 return number
             else:
                 count += 1
-        number +=1
+        number += 2
 
 
 if __name__ == '__main__':
-    print(prime())
+    t = int(input().strip())
+    prime_dict = {}
+    prime_list = [2]
+    for a0 in range(t):
+        num = int(input().strip())
+        print(prime(num, prime_list))
