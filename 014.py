131bc429e0497851c8159dba4d1aecc4c656795c
yashasvi <yashpant20@gmail.com>
2020-05-11 19:15:10 +0545

    optimized for hackerrank

--- 014.py
+++ 014.py
@@ -22,23 +22,47 @@ NOTE: Once the chain starts the terms are allowed to go above one million.
 """
 
 
-def get_count():
-    st_count_dict = {}
-    for starting_num in range(1, 1000000):
+def get_count(N, count_list, beloworequal_n_highest_count, beloworequal_n_highest_number):
+    length = len(beloworequal_n_highest_count)
+
+    if length >= N:
+        return beloworequal_n_highest_number[N - 1]
+    else:
+        starting_range = length + 1
+        highest_count = beloworequal_n_highest_count[length - 1]
+        reqd_num = beloworequal_n_highest_number[length - 1]
+    for starting_num in range(starting_range, N + 1):
         num = starting_num
-        count = 1
+        cou = 1
         while num != 1:
-            if num in st_count_dict:
-                count += st_count_dict[num]
+            if num <= len(count_list):
+                cou += count_list[num - 1] - 1
                 break
             if num % 2 == 0:
-                num /= 2
+                num //= 2
             else:
                 num = 3 * num + 1
-            count += 1
-        st_count_dict[starting_num] = count
-    return max(st_count_dict, key=st_count_dict.get)
+            cou += 1
+        count_list.append(cou)
+        highest_count_temp = highest_count
+        if highest_count <= cou:
+            reqd_num = starting_num
+            highest_count = cou
+            beloworequal_n_highest_count.append(highest_count)
+            beloworequal_n_highest_number.append(starting_num)
+        else:
+            beloworequal_n_highest_count.append(highest_count_temp)
+            beloworequal_n_highest_number.append(reqd_num)
+
+    return reqd_num
 
 
 if __name__ == '__main__':
-    print(get_count())
+    count = int(input().strip())
+    st_count_list = [1]
+    beloworequal_n_highest = [1]
+    beloworequal_n_highest_num = [1]
+
+    for i in range(count):
+        n = int(input().strip())
+        print(get_count(n, st_count_list, beloworequal_n_highest, beloworequal_n_highest_num))
